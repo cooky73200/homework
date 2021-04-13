cd4f6d794ad03e2556ec78a00eab5902ff5b1327
cooky73200 <cooky73200@gmail.com>
2021-04-13 19:21:44 +0800

    add count

--- import.py
+++ import.py
@@ -4,16 +4,19 @@ s = input('請輸入下限 ')
 b = int(b)
 s = int(s)
 r = random.randint(s,b)
-
+count = 0
 while True:
 	num = input('輸入數字 ')
 	num = int(num)
-	if num < r:
-		print('比答案小')
-	elif num > r:
-		print('比答案大')
+	if num <= b and num >= s:
+		count += 1
+		if num < r:
+			print('比答案小')
+		elif num > r:
+			print('比答案大')
+		else:
+			print('答對了 ','你總共猜了', count, '次')
+			break
 	else:
-		print('答對了')
-		break
-				
+		print('是不是在哭')	
 
